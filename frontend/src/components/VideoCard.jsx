3e25f968db1475d415476eff37ffd206e6ef7404
Mayank Rawat <rmayank451@gmail.com>
2026-06-11 23:42:12 +0530

    Add video card navigation to video player page

--- frontend/src/components/VideoCard.jsx
+++ frontend/src/components/VideoCard.jsx
@@ -1,27 +1,31 @@
+import { Link } from "react-router-dom";
+
 const VideoCard = ({ video }) => {
-  return (
-    <div className="cursor-pointer">
-      <img
-        src={video.thumbnailUrl}
-        alt={video.title}
-        className="h-52 w-full rounded-xl object-cover"
-      />
+    return (
+        <Link to={`/video/${video._id}`}>
+            <div className="cursor-pointer">
+                <img
+                    src={video.thumbnailUrl}
+                    alt={video.title}
+                    className="h-52 w-full rounded-xl object-cover"
+                />
 
-      <div className="mt-3">
-        <h3 className="font-semibold">
-          {video.title}
-        </h3>
+                <div className="mt-3">
+                    <h3 className="font-semibold">
+                        {video.title}
+                    </h3>
 
-        <p className="text-sm text-gray-600">
-          {video.channelId?.channelName}
-        </p>
+                    <p className="text-sm text-gray-600">
+                        {video.channelId?.channelName}
+                    </p>
 
-        <p className="text-sm text-gray-600">
-          {video.views} views
-        </p>
-      </div>
-    </div>
-  );
+                    <p className="text-sm text-gray-600">
+                        {video.views} views
+                    </p>
+                </div>
+            </div>
+        </Link>
+    );
 };
 
 export default VideoCard;
